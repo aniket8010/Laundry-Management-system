df87efa207be313cb144c7558e38d9a8a14983b8
akshaysolanke <akshayashoksolanke@gmail.com>
2025-02-18 18:51:02 +0530

    registrationform

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,41 +4,43 @@ import "./responsive.css";
 
 function home() {
   return (
-    <div style={{backgroundColor:"#F3F3F3"}} className="session ">
+    <div style={{ backgroundColor: "#F3F3F3" }} className="session ">
       <div className="container">
-      <div className="row">
-        <div className="col-12 col-lg-6">
-          <div className="home_banner m-auto">
-            <img
-              src="https://assets.ucleanlaundry.com/assets/images/service_main_image_11zon.webp"
-              alt=""
-            />
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <div className="home_banner m-auto">
+              <img
+                src="https://assets.ucleanlaundry.com/assets/images/service_main_image_11zon.webp"
+                alt=""
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-12 col-lg-6">
-          <div className="a_h5">
-          <h5>LAUNDRY</h5>
+          <div className="col-12 col-lg-6">
+            <div className="a_h5">
+              <h5>LAUNDRY</h5>
+            </div>
+            <p>
+              Customer satisfaction is at the heart of everything we do. With a
+              team of <br /> experienced professionals, modern equipment, and a
+              commitment <br /> to excellence, we aim to make laundry day
+              stress-free for you. Experience <br /> the difference with Rutuja
+              Laundry and Drycleaners because your clothes <br /> deserve the
+              best..!
+            </p>
+            <div className="a_order">
+              <span>Create Your Order..!</span>
+            </div>
+            <div className="a_button">
+              <a href="Registration">
+                <button>ORDER NOW</button>
+              </a>
+            </div>
+            <div className="a_place">
+              <h6>Place Your Order Now</h6>
+            </div>
           </div>
-          <p>
-            Customer satisfaction is at the heart of everything we do. With a
-            team of <br /> experienced professionals, modern equipment, and a
-            commitment <br /> to excellence, we aim to make laundry day stress-free for
-            you. Experience <br /> the difference with Rutuja Laundry and Drycleaners because
-            your clothes <br /> deserve the best..!
-          </p>
-          <div className="a_order">
-            <span>Create Your Order..!</span>
-            
-          </div>
-         <div className="a_button">
-          <a href="Registration"><button>ORDER NOW</button></a>
-         </div>
-         <div className="a_place">
-          <h6>Place Your Order Now</h6>
-         </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import "./registration.css"
+
 
 function registration() {
   return (
-    <div>
-      RegistratioN
+    <div className="container">
+      {/* registration form */}
+      <div className="a_box">
+      <div class="form-box" id="register">
+      <h2>Register/Login</h2>
+      <form>
+          <input type="text" placeholder="Full Name" required/>
+          <input type="email" placeholder="Email" required/>
+          <input type="text" placeholder="contact.no" required/>
+          <input type="text" placeholder="current Address" required/>
+          <input type="password" placeholder="Password" required/>
+          <button type="submit">Register</button>
+          <p>Already have an account?<a href="#" onclick="showLogin()">Login</a> </p>
+      </form>
+      </div>
 
+
+      {/* login from */}
+      <div class="form-box" id="login" style={{display:"none"}}>
+      <h2>Login</h2>
+            <form>
+                <input type="email" placeholder="Email" required/>
+                <input type="password" placeholder="Password" required/>
+                <button type="submit">Login</button>
+                <p>Don't have an account<a href="#" onclick="showRegister()">Register</a></p>
+            </form>
+      </div>
+      </div>
     </div>
+    
   )
+  
 }
 
 export default registration
